975ce3dfd87d3765e920ffe7cccdb95ff8572909
Mate Marko <markomate90@gmail.com>
2021-03-29 17:22:58 +0200

    setting up the project

---
+++ src/test/java/com/ky0j1n/greenbay/GreenbayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ky0j1n.greenbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenbayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
